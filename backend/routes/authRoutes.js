40c3389957d85f792b197a3ea334a1209a952968
Naman Jain <namanjain01508@gmail.com>
2025-09-28 17:02:48 +0530

    Add Accident Reporting UI scaffolding and backend adjustments

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express, { Router } from "express";
 import { registerUser,loginUser } from "../controllers/auth.controllers.js";
+import { registerValidation,loginValidation } from "../validations/authValidations.js";
+import { validateRequest } from "../middleware/validateRequest.middleware.js";
 
 const router = express.Router();
 
-router.post("/register",registerUser);
-router.post("/login",loginUser);
+router.post("/register",registerUser,validateRequest,registerValidation);
+router.post("/login",loginUser,validateRequest,loginValidation);
 
 export default router;
